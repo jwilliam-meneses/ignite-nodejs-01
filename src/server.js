e3bb5ae3fe8c1888813e19f69dae89915bbf206f
José William <jwmeneses@sfiec.org.br>
2024-03-06 13:42:17 -0300

    Separa as rotas da aplicação

--- src/server.js
+++ src/server.js
@@ -1,39 +1,22 @@
 import http from 'node:http';
-import { randomUUID } from 'node:crypto';
 import { json } from './middlewares/json.js';
-import { Database } from './database.js';
-
-const database = new Database;
+import { routes } from './routes.js';
 
 const server = http.createServer(async (req, res) => {
     const { method, url } = req;
 
     await json(req, res);
 
-    if (method === 'GET' && url === '/users') {
-        const users = database.select('users');
+    const route = routes.find(route => {
+        return route.method === method && route.path === url;
+    });
 
-        return res
-            .end(JSON.stringify(users));
-    }
-
-    if (method === 'POST' && url === '/users') {
-        const { name, email } = req.body;
-
-        const user = {
-            id: randomUUID(),
-            name,
-            email
-        };
-
-        database.insert('users', user);
-
-        return res.writeHead(201).end();
+    if (route) {
+        return route.handle(req, res);
     }
 
     res.writeHead(404).end();
 });
 
-const url = 'http://localhost';
 const port = 3333;
-server.listen(port, () => console.log(`Servidor rodando em ${url}:${port}`));
+server.listen(port);
